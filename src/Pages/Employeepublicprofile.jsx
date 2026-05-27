b82f3d3efcebac2b9e80af031093550e60a22207
C-TechIT25 <contact@ctechengineering.in>
2026-05-27 11:47:15 +0530

    change

--- src/Pages/Employeepublicprofile.jsx
+++ src/Pages/Employeepublicprofile.jsx
@@ -6,7 +6,7 @@ import JsBarcode from "jsbarcode";
 
 // MUI Core
 import {
-  Box, Typography, Avatar, Chip, Paper, Grid,ThemeProvider,
+  Box, Typography, Avatar, Chip, Paper, Grid,
   CircularProgress, Container, Badge, Button,
 } from "@mui/material";
 import { createTheme, ThemeProvider, alpha } from "@mui/material/styles";
